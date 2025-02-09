db7bdb4f7e19d403c8cab3d11eecc30bbabf0994
seniru-dilmith <dilmithseniru@gmail.com>
2025-02-09 21:25:32 +0530

    minor change

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -15,7 +15,7 @@ export default function Custom404() {
         <h1 className="text-7xl font-extrabold text-accent">404</h1>
         <p className="text-xl md:text-2xl font-semibold mt-4">Oops! Page not found</p>
         <p className="text-gray-500 mt-2">
-          The page you're looking for doesn't exist or has been moved.
+          The page you&rsquo;re looking for doesn&rsquo;t exist or has been moved.
         </p>
 
         {/* Illustration */}
