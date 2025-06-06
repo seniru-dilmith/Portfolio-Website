be39116656cd7a2b88fdfa0b585b030f1ea56c31
Seniru Dilmith <dilmithseniru@gmail.com>
2025-06-06 20:31:51 +0530

    Fix dbConnect test by setting env var

--- __tests__/util/dbConnect.test.ts
+++ __tests__/util/dbConnect.test.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
-import dbConnect from "@/util/dbConnect";
 
 jest.mock("mongoose", () => ({
   connect: jest.fn(() => Promise.resolve({ connection: {} })),
 }));
 
 describe("dbConnect", () => {
+  beforeAll(() => {
+    process.env.NEXT_MONGO_URI = "mongodb://localhost/test";
+  });
+
   it("connects using mongoose", async () => {
+    const { default: dbConnect } = await import("@/util/dbConnect");
     const conn = await dbConnect();
     expect(mongoose.connect).toHaveBeenCalled();
     expect(conn).toBeDefined();
