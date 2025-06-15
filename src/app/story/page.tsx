4c63fe0d439ac3708a3e1b544d7d99c968d6968b
seniru-dilmith <dilmithseniru@gmail.com>
2025-06-15 14:45:38 +0530

    remove hover scale effect from Story component for improved performance

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -22,9 +22,6 @@ const Story: React.FC = () => {
         variants={containerVariants}
       >
         <motion.div
-          whileHover={{
-            scale: 1.02,
-          }}
           transition={{ type: "spring", stiffness: 300, damping: 30 }}
         >
           <UpperImageSection />
