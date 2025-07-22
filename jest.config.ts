6de34945827d568253062311ab15c5c780fd1ec7
seniru-dilmith <dilmithseniru@gmail.com>
2025-07-22 23:45:48 +0530

    change test folder structure
    - moved ftest files to the original file directory

--- jest.config.ts
+++ jest.config.ts
@@ -12,17 +12,17 @@ const customJestConfig: Config = {
   testEnvironment: 'jest-environment-jsdom',
   
   moduleNameMapper: {
-    // This alias is correct for a project with a 'src' directory.
+    // project with a 'src' directory
     '^@/(.*)$': '<rootDir>/src/$1',
   },
 
-  // This explicitly tells Jest to use babel-jest to transform all JS/TS files.
+  // use babel-jest to transform all JS/TS files
   transform: {
-    // The `as const` assertion tells TypeScript this is a tuple, not a generic array.
+    // tuple, not a generic array
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }] as const,
   },
 
-  // We still need this to tell Jest *which* node_modules to transform.
+  // which node_modules to transform
   transformIgnorePatterns: [
       '/node_modules/(?!remark|react-markdown|unified|unist-.*|hast-.*|bail|trough|vfile.*|micromark.*|decode-named-character-reference|character-entities|mdast-util-.*|escape-string-regexp|markdown-table|github-slugger|strip-markdown|remark-gfm)/',
       '^.+\\.module\\.(css|sass|scss)$',
